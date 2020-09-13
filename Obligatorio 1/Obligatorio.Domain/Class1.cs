3e5044048ad21993257afc5c1b14fb0f236e4580
JuanP539 <jpbarriosgarcia@gmail.com>
2020-09-13 13:05:10 -0300

    Creacion de la clase TouristSpot, junto a sus atributos, metodos basicos y pruebas

--- Obligatorio 1/Obligatorio.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Obligatorio.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Obligatorio 1/Obligatorio.Domain/TouristSpot.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+namespace Obligatorio.Domain
+{
+    public class TouristSpot
+    {
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Image { set; get; }
+        
+        public TouristSpot()
+        {
+            Name = "Default Name";
+            Description = "Default Description";
+            Image = "Default Image";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TouristSpot spot &&
+                   Name == spot.Name;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("Tourist Spot called {0}, and its description is {1}.",
+                Name, Description);
+            if (Image == "Default Image")
+            {
+                s += "And has no image.";
+            }
+            else 
+            {
+                s += "And has an image.";
+            }
+            return s;
+        }
+    }
+}
